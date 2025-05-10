e35b40a213d44834e45e667c9afbab2ac107ec4d
vikumarks <vikumar7ks@gmail.com>
2025-05-09 18:35:45 -0700

    "show platform temp" unit test added

--- tests/show_platform_test.py
+++ tests/show_platform_test.py
@@ -51,6 +51,26 @@ class TestShowPlatform(object):
                 assert result.output == textwrap.dedent(expected_output)
 
 
+class TestShowPlatformTemperature(object):
+    """
+        Note: `show platform temperature` simply calls the `tempershow` utility and
+        passes a variety of options. Here we test that the utility is called
+        with the appropriate option(s). The functionality of the underlying
+        `tempershow` utility is expected to be tested by a separate suite of unit tests
+    """
+    def test_temperature(self):
+        with mock.patch('utilities_common.cli.run_command') as mock_run_command:
+            CliRunner().invoke(show.cli.commands['platform'].commands['temperature'], [])
+        assert mock_run_command.call_count == 1
+        mock_run_command.assert_called_with(['tempershow'], display_cmd=False)
+
+    def test_all_temperature_json(self):
+        with mock.patch('utilities_common.cli.run_command') as mock_run_command:
+            CliRunner().invoke(show.cli.commands['platform'].commands['temperature'], ['--json'])
+        assert mock_run_command.call_count == 1
+        mock_run_command.assert_called_with(['tempershow', '-j'], display_cmd=False)
+
+
 class TestShowPlatformPsu(object):
     """
         Note: `show platform psustatus` simply calls the `psushow` utility and
